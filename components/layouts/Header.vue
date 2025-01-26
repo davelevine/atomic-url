e05e50af8d24318cb59fd4f5ab6454985d657773
davelevine <dave@davelevine.io>
2025-01-26 15:00:12 -0500

    refactor(header): simplify mobile navigation
    - Remove dropdown menu functionality on mobile
    - Keep theme switcher visible and right-aligned in header
    - Maintain desktop navigation unchanged

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -14,6 +14,7 @@ const { title, github } = useAppConfig()
       <div
         class="container relative flex flex-wrap items-center justify-between h-24 px-0 mx-auto overflow-hidden font-medium border-b border-border md:overflow-visible lg:justify-center"
       >
+        <!-- Main header title -->
         <div class="flex items-center justify-start w-1/4 h-full pr-4">
           <a
             href="/"
@@ -33,35 +34,18 @@ const { title, github } = useAppConfig()
           </a>
         </div>
 
-        <!-- Add theme switcher for mobile view -->
-        <div class="flex items-center md:hidden mr-12">
+        <!-- Mobile theme switcher -->
+        <div class="flex items-center md:hidden">
           <SwitchTheme />
         </div>
 
+        <!-- Desktop menu -->
         <div
-          class="top-0 left-0 items-start w-full h-full p-4 text-sm bg-background bg-opacity-50 md:items-center md:w-3/4 md:absolute lg:text-base md:bg-transparent md:p-0 md:relative md:flex"
-          :class="{ 'flex fixed': showMenu, 'hidden': !showMenu }"
-          @touchmove.prevent
+          class="hidden md:flex top-0 left-0 items-start w-full h-full p-4 text-sm bg-background md:items-center md:w-3/4 md:absolute lg:text-base md:bg-transparent md:p-0 md:relative"
         >
           <div
             class="flex-col w-full h-auto overflow-hidden rounded-lg bg-background md:overflow-visible md:rounded-none md:relative md:flex md:flex-row"
           >
-            <a
-              href="/"
-              :title="title"
-              class="inline-flex items-center w-auto h-16 px-4 text-xl font-black leading-none text-foreground md:hidden whitespace-nowrap"
-            >
-              <span
-                class="flex items-center justify-center w-8 h-8 text-white bg-primary rounded-full"
-              >
-                <img
-                  src="/sink.png"
-                  :alt="title"
-                  class="w-full h-full rounded-full"
-                >
-              </span>
-              <span class="mx-2">{{ title }}</span>
-            </a>
             <div class="w-full mx-4" />
             <div
               class="flex flex-col items-start justify-end w-full pt-4 md:items-center md:w-1/3 md:flex-row md:py-0"
@@ -86,21 +70,6 @@ const { title, github } = useAppConfig()
               </span>
             </div>
           </div>
-        </div>
-
-        <div
-          class="absolute right-0 flex flex-col items-center justify-center w-10 h-10 rounded-full cursor-pointer md:hidden hover:bg-accent"
-          :class="{ 'right-2': showMenu }"
-          @click="showMenu = !showMenu"
-        >
-          <Ellipsis
-            v-show="!showMenu"
-            class="w-6 h-6"
-          />
-          <X
-            v-show="showMenu"
-            class="w-6 h-6"
-          />
         </div>
       </div>
     </nav>
